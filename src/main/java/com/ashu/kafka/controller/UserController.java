838d8bcaf0b86cc5ed238959f6e0f5ee2b0254bc
Ashu <ashu.ask.singh@gmail.com>
2023-02-18 23:13:57 +0530

    working with non-primitive data

--- src/main/java/com/ashu/kafka/controller/UserController.java
+++ src/main/java/com/ashu/kafka/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.ashu.kafka.controller;
 
+import com.ashu.kafka.dto.User;
 import com.ashu.kafka.service.UserProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/userapi")
@@ -14,8 +13,13 @@ public class UserController {
     @Autowired
     private UserProducerService userProducerService;
 
-    @PostMapping("/ashuData/{name}/{age}")
-    public void sendUserData(@PathVariable("name") String name, @PathVariable("age") Integer age) {
-        userProducerService.sendUserData(name, age);
+//    @PostMapping("/ashuData/{name}/{age}")
+//    public void sendUserData(@PathVariable("name") String name, @PathVariable("age") Integer age) {
+//        userProducerService.sendUserData(name, age);
+//    }
+
+    @PostMapping("/ashuData")
+    public void sendUserData(@RequestBody User user) {
+        userProducerService.sendUserData(user);
     }
 }
--- src/main/java/com/ashu/kafka/service/UserProducerService.java
+++ src/main/java/com/ashu/kafka/service/UserProducerService.java
@@ -1,5 +1,6 @@
 package com.ashu.kafka.service;
 
+import com.ashu.kafka.dto.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class UserProducerService {
 
     @Autowired
-    private KafkaTemplate<String, Integer> kafkaTemplate;
+//    private KafkaTemplate<String, Integer> kafkaTemplate;
+    private KafkaTemplate<String, User> kafkaTemplate;
 
-    public void sendUserData(String name, Integer age) {
-        kafkaTemplate.send("user-topic", name, age);
+//    public void sendUserData(String name, Integer age) {
+    public void sendUserData(User user) {
+        kafkaTemplate.send("user-topic", user.getName(), user);
     }
 }
